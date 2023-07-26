5f0c59731ee807796287533f1240590ae5660f09
chennakesava <chennakesavulukallagunta@gmail.com>
2023-07-26 19:55:12 +0530

    updated payment scripting newely added set command

--- 04,varable.sh
+++ 04,varable.sh
@@ -2,3 +2,12 @@ a=10
 echo value of a is $a dollars
 #Alternativeley variable also can be acessed by using ${a}
 echo value of a is ${a}dollars
+
+#special variables for inputs
+#special variables used for inputs are $0 to $n, $*, $#
+
+echo $0
+echo $1
+echo $*
+echo $#
+
