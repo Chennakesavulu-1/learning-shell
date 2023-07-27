992bb4cd61f99e8a9cfe002a3d4dcab70d356f28
chennakesava <chennakesavulukallagunta@gmail.com>
2023-07-27 07:43:08 +0530

    loops concept, for

--- 06.loops.sh
+++ 06.loops.sh
@@ -3,3 +3,8 @@ while [ $a -gt 0 ]; do
   echo Hello World
   a=$(($a-1))
 done
+
+for component in catalogue user cart frontend shipping payment ; do
+  echo creating component -$component
+  sleep 1
+done
